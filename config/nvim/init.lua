dba6ce0b743c10bea63168659a4f8dc6c36206e6
Guido Woller <guido.woller@icloud.com>
2026-03-30 13:10:44 +0200

    update mac setup (GuidoDock.fim.uni-passau.de)

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -23,5 +23,21 @@ require("lazy").setup({
 vim.o.background = "dark"
 vim.cmd("colorscheme gruvbox")
 
+-- line numbers
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.api.nvim_create_autocmd("InsertEnter", {
+  callback = function()
+    vim.o.relativenumber = false
+  end,
+})
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+  callback = function()
+    vim.o.relativenumber = true
+  end,
+})
+
 -- lualine
 require("lualine").setup()
