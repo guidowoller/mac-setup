2bc51a4be211b61fc8e8836404dbc1e695474f2a
Guido Woller <guido.woller@icloud.com>
2026-04-30 16:59:52 +0200

    update mac setup (MacBookGuido.fritz.box)

--- scripts/a.sh
+++ scripts/a.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#ssh cervicales.fim.uni-passau.de -t "tmux attach -t uni || tmux new -s uni"
-autossh -M 0 -t guido@cervicales.fim.uni-passau.de "tmux attach || tmux new"
+ssh cervicales.fim.uni-passau.de -t "tmux attach -t uni || tmux new -s uni"
+#autossh -M 0 -t guido@cervicales.fim.uni-passau.de "tmux attach || tmux new"
